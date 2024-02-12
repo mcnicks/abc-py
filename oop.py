f26170a16e968725ec7a84382998f72915c4a127
mcnick <mcnick.perm@gmail.com>
2024-02-12 15:51:57 +0500

    descriptors

--- oop.py
+++ oop.py
@@ -1,6 +1,22 @@
+class TestDescr:
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+        print(f'{self.name=}')
+
+    def __set__(self, instance, value):
+        print(f'setter: {value=}')
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, owner):
+        print(f'getter: {self.name=}')
+        return instance.__dict__[self.name]
+
 
 class TestClass:
     int_a = 1
+
+    desc_a = TestDescr()
 
     @classmethod
     def set_a(cls, a):
@@ -14,4 +30,7 @@ class TestClass:
 
     def __init__(self, a):
         self.a = a
+        self.desc_a = a
 
+e = TestClass(5)
+print(e.desc_a)
